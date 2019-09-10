c3084e4205383dfb39cc1ffd6267e3e5caf47730
Rafael <rdominguez@awm.do>
2019-09-10 15:08:12 -0400

    Initial Commit

---
+++ AWMTest01/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AWMTest01
+//
+//  Created by AWM Solutions on 9/10/19.
+//  Copyright © 2019 AWM Solutions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AWMTest01/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AWMTest01
+//
+//  Created by AWM Solutions on 9/10/19.
+//  Copyright © 2019 AWM Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
